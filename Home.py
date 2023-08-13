327efc154e3de7d13cdda598f792c6be29f349c7
William Jonas <57875705+WilliamJonas08@users.noreply.github.com>
2023-08-13 21:34:51 +0200

    remove home css styling

--- Home.py
+++ Home.py
@@ -7,9 +7,6 @@ st.set_page_config(
     page_icon="🌍",
 )
 
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
-
 st.session_state['language'] = 'FR'
 
 with open('README.md', 'r') as f:
