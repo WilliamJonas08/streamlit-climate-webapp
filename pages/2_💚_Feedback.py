6323710fa6b2928869ab1294bea0a7e09234b1d5
William Jonas <57875705+WilliamJonas08@users.noreply.github.com>
2023-08-13 22:56:18 +0200

    add feedback panel

---
+++ pages/2_💚_Feedback.py
@@ -0,0 +1,22 @@
+# Inspired from https://www.youtube.com/watch?v=FOULV9Xij_8
+
+import streamlit as st  # pip install streamlit
+
+with open('style/feedback.css') as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+
+st.write("# :mailbox: Get In Touch With Me!")
+st.write("Any feedback will be appreciated 💚")
+
+contact_form = """
+<form action="26393e880afb441edff4c4baaa47e867" method="POST">
+     <input type="hidden" name="_captcha" value="false">
+     <input type="text" name="name" placeholder="Your name" required>
+     <input type="email" name="email" placeholder="Your email" required>
+     <textarea name="message" placeholder="Your feedback here"></textarea>
+     <button type="submit">Send</button>
+</form>
+"""
+
+st.markdown(contact_form, unsafe_allow_html=True)
