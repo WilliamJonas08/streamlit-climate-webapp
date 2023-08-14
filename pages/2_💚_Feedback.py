6de1a511212bd3c9197c31e5cddf21eddccd5440
William Jonas <57875705+WilliamJonas08@users.noreply.github.com>
2023-08-14 18:54:02 +0200

    add alternative button link for feedback

--- pages/2_💚_Feedback.py
+++ pages/2_💚_Feedback.py
@@ -10,17 +10,23 @@ with open('style/feedback.css') as f:
 st.write("# :mailbox: Get In Touch With Me!")
 st.write("Any feedback will be appreciated 💚")
 
-contact_form = """
-<form action="26393e880afb441edff4c4baaa47e867" method="POST">
-     <input type="hidden" name="_captcha" value="false">
-     <input type="text" name="name" placeholder="Your name" required>
-     <input type="email" name="email" placeholder="Your email" required>
-     <textarea name="message" placeholder="Your feedback here"></textarea>
-     <button type="submit">Send</button>
-</form>
+
+# Not working for now : 403 Forbidden acces
+# contact_form = """
+# <form action="26393e880afb441edff4c4baaa47e867" method="POST">
+#      <input type="hidden" name="_captcha" value="false">
+#      <input type="text" name="name" placeholder="Your name" required>
+#      <input type="email" name="email" placeholder="Your email" required>
+#      <textarea name="message" placeholder="Your feedback here"></textarea>
+#      <button type="submit">Send</button>
+# </form>
+# """
+# st.markdown(contact_form, unsafe_allow_html=True)
+
+# Using the alternative for now
+contact_form2 = """
+<br>
+<a href="https://formsubmit.co/el/nabolo" class="send-bttn">Send Feedback</a>
 """
+st.markdown(contact_form2, unsafe_allow_html=True)
 
-st.markdown(contact_form, unsafe_allow_html=True)
-
-with st.container():
-    st.markdown(f"If the form above doesn't work, you can try with this (secured) link: https://formsubmit.co/el/nabolo")
